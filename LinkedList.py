abfe0b4f4a3585b7c880f0f8c93a7b9b23baa5da
Xavier Benitez <117019646+xbenitez1@users.noreply.github.com>
2023-08-17 18:11:21 +0200

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -3,18 +3,19 @@ class Node:
         self.value = value
         self.next = None
 
+
 class LinkedList:
     # def __init__(self, value):
     #     new_node = Node(value)
     #     self.head = new_node
     #     self.tail = new_node
     #     self.length = 1
-    
+
     def __init__(self):
         self.head = None
         self.tail = None
         self.length = 0
-    
+
     def __str__(self):
         temp_node = self.head
         result = ''
@@ -24,7 +25,7 @@ class LinkedList:
                 result += ' -> '
             temp_node = temp_node.next
         return result
-    
+
     def append(self, value):
         new_node = Node(value)
         if self.head is None:
@@ -34,7 +35,7 @@ class LinkedList:
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
-    
+
     def prepend(self, value):
         new_node = Node(value)
         if self.head is None:
@@ -44,7 +45,7 @@ class LinkedList:
             new_node.next = self.head
             self.head = new_node
         self.length += 1
-    
+
     def insert(self, index, value):
         new_node = Node(value)
         if self.head is None:
@@ -55,18 +56,18 @@ class LinkedList:
             self.head = new_node
         else:
             temp_node = self.head
-            for _ in range(index-1):
+            for _ in range(index - 1):
                 temp_node = temp_node.next
             new_node.next = temp_node.next
             temp_node.next = new_node
         self.length += 1
-    
+
     def traverse(self):
         current = self.head
         while current is not None:
             print(current.value)
             current = current.next
-    
+
     def search(self, target):
         current = self.head
         while current is not None:
@@ -74,7 +75,7 @@ class LinkedList:
                 return True
             current = current.next
         return False
-    
+
     def search(self, target):
         current = self.head
         index = 0
@@ -84,7 +85,7 @@ class LinkedList:
             current = current.next
             index += 1
         return -1
-    
+
     def get(self, index):
         if index == -1:
             return self.tail
@@ -94,14 +95,14 @@ class LinkedList:
         for _ in range(index):
             current = current.next
         return current
-    
+
     def set_value(self, index, value):
         temp = self.get(index)
         if temp:
             temp.value = value
             return True
         return False
-    
+
     def pop_first(self):
         if self.length == 0:
             return None
@@ -114,10 +115,8 @@ class LinkedList:
             popped_node.next = None
         self.length -= 1
         return popped_node
-    
-    
-    
-    def pop(self):
+
+    def pop_last(self):
         if self.length == 0:
             return None
         popped_node = self.tail
@@ -131,27 +130,22 @@ class LinkedList:
             self.tail = temp
         self.length -= 1
         return popped_node
-    
 
     def remove(self, index):
         if index < -1 or index >= self.length:
             return None
         if index == 0:
             return self.pop_first()
-        if index == -1 or index == self.length-1:
+        if index == -1 or index == self.length - 1:
             return self.pop()
-        prev_node = self.get(index-1)
+        prev_node = self.get(index - 1)
         popped_node = prev_node.next
         prev_node.next = popped_node.next
         popped_node.next = None
         self.length -= 1
         return popped_node
-    
 
-    def remove(self, index):
-        prev_node = self.get(index-1)
-        popped_node = prev_node.next
-        prev_node.next = popped_node.next
-        popped_node.next = None
-        self.length -= 1
-        return popped_node
+    def delete_all(self):
+        self.head = None
+        self.tail = None
+        self.length = None
